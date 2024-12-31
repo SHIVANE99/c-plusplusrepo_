78f1dc9e55490656d04bb5d3aeb0204192330aae
SHIVANE99 <shivanepadhapc@gmail.com>
2024-12-31 22:06:13 +0530

    code9

---
+++ code9.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1;
+    int num2;
+    int sum;
+    cout<<"enter 2 numbers";
+    cin>>num1>>num2;
+    sum=num1+num2;
+    cout<<"sum of 2 numbers are :"<<sum;
+    
+}
