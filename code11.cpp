87b917c0746c44a7ae96b6b544dc094a3c796135
SHIVANE99 <shivanepadhapc@gmail.com>
2025-01-03 21:44:40 +0530

    code11

---
+++ code11.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<< "size of integer is "<<sizeof(int)<<endl;
+    cout<< "size of character is "<<sizeof(char)<<endl;
+    cout<< "size of double is "<<sizeof(double) <<endl;
+    cout<< "size of float is "<<sizeof(float)   <<endl;
+
+}
