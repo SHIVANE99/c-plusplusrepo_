7f398a00e67c950b359037d6e3192aa7763ef295
SHIVANE99 <shivanepadhapc@gmail.com>
2025-01-04 20:40:27 +0530

    code12

---
+++ code12.cpp
@@ -0,0 +1,19 @@
+//swapping 2 numbers
+#include<iostream>
+using namespace std;
+int a;
+int b;
+int temp;
+int main(){
+    cout<< "enter 1st number :";
+    cin>>a;
+    cout<<"enter 2nd number";
+    cin>>b;
+    temp=a;
+    a=b;
+    b=temp;
+    cout<<"after swapping the numbers are :"<<a<<","<<b<<endl;
+
+
+
+}
