d4588ff676f1b8cc29753784e599437f9c7d9269
SHIVANE99 <shivanepadhapc@gmail.com>
2025-01-17 11:37:51 +0530

    code25

---
+++ code25.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"square\t\tcube"<<endl;
+    cout<<"1*1\t\t1*1*1"<<endl;
+    cout<<"2*2\t\t2*2*2"<<endl;
+    cout<<"3*3\t\t3*3*3"<<endl;
+}
+
