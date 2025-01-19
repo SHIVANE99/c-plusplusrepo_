cb696a8f3fb286f552f1f6dee62b6fab27df1019
SHIVANE99 <shivanepadhapc@gmail.com>
2025-01-19 14:27:38 +0530

    cc

--- code30.cpp
+++ code30.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 using namespace std;
+//understanding floating point default printing format
 int main(){
     cout<<123124.56<<'\n';
-    cout<<1.000000F;
+    cout<<1.000000;
 
 }
---
+++ code31.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    double a=21442234.0000;
+    cout<<setprecision(4);
+    double x=123.567;
+    double y=342553.5657;
+    cout<<x<<" "<<y<<endl;
+    cout<<showpoint<<a<<endl;
+    cout<<uppercase<<a;
+
+}
