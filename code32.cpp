a052980f83d4e83a128281f3233e5e56bd9788d8
SHIVANE99 <shivanepadhapc@gmail.com>
2025-01-19 15:28:20 +0530

    up

---
+++ code32.cpp
@@ -0,0 +1,19 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+    // Print S and move cursor to a new line
+    cout<<s,'\n';
+
+    return 0;
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
